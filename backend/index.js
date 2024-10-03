7cbdeb6498470641e74c5f0478bee72f805a2406
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-03 17:27:14 -0400

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
-import connectDb from './Database/db.js';
-
+import cors from "cors";
+import connectDb from "./Database/db.js";
 
 dotenv.config();
 
@@ -11,18 +10,32 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-
-app.use(cors());
-
+// app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 //using middleweares
-app.use(express.json());  // data pass for register and login
-app.use(cookieParser());  // npm cookie cookies pass for profile
- 
+app.use(express.json()); // data pass for register and login
+app.use(cookieParser()); // npm cookie cookies pass for profile
+
 // import routing
 
 
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    connectDb();
-})
+  console.log(`Server is running on http://localhost:${port}`);
+  connectDb();
+});
