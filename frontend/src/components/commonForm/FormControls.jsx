8ff0b1234a819589d02041aa87e4b5dc67c9625f
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-03 19:19:47 -0400

    minor updates

---
+++ frontend/src/components/commonForm/FormControls.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FormControls = ({formControls = [], formData, setFormData }) => {
+    
+  return (
+    <div className='flex flex-col gap-3'>
+      {}
+    </div>
+  )
+}
+
+export default FormControls
