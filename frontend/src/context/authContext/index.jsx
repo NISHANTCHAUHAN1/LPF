a32e844807b2919e6461846114881d3b203e2f55
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-05 17:14:22 -0400

    minor updates

--- frontend/src/context/authContext/index.jsx
+++ frontend/src/context/authContext/index.jsx
@@ -1,23 +1,70 @@
 import { initialSignInFormData, initialSignUpFormData } from "@/config";
-import { loginService, registerService } from "@/services";
-import { createContext, useState } from "react";
+import { checkAuthService, loginService, registerService } from "@/services";
+import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
   const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
   const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
+  const [auth, setAuth] = useState({
+    authenticate: false,
+    user: null,
+  });
 
-  const handleRegister = async(e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
     const data = await registerService(signUpFormData);
     // console.log(data);
-  }
+  };
 
-  const handleLoginUser = async(e) => {
+  const handleLoginUser = async (e) => {
     e.preventDefault();
     const data = await loginService(signInFormData);
-    console.log(data);
-  }
+    // console.log(data);
+    if (data.success) {
+      sessionStorage.setItem(
+        "accessToken",
+        JSON.stringify(data.data.accessToken)
+      );
+      setAuth({
+        authenticate: true,
+        user: data.data.user,
+      });
+    } else {
+      setAuth({
+        authenticate: false,
+        user: null,
+      });
+    }
+  };
+
+  // check auth
+  const checkAuthUser = async () => {
+    try {
+      const data = await checkAuthService();
+      if (data.success) {
+        setAuth({
+          authenticate: true,
+          user: data.data.user,
+        });
+      } else {
+        setAuth({
+          authenticate: false,
+          user: null,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthUser();
+  }, []);
+
+  console.log(auth);
+  
+
 
   return (
     <AuthContext.Provider
