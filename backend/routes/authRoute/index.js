6c63d4993646f3dc6286ca0dab55e3b3d37a0acb
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-05 13:05:48 -0400

    minor updates

--- backend/routes/authRoute/index.js
+++ backend/routes/authRoute/index.js
@@ -1,10 +1,25 @@
 import express from "express";
-import { loginUser, register } from "../../controllers/authController/authCont.js";
+import {
+  loginUser,
+  register,
+} from "../../controllers/authController/authCont.js";
+import authenticate from "../../middleware/authmid.js";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', loginUser);
+router.post("/register", register);
+router.post("/login", loginUser);
 
+router.post("/chech-auth", authenticate, (req, res) => {
+  const user = req.user;
+
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user!",
+    data: {
+      user,
+    },
+  });
+});
 
 export default router;
