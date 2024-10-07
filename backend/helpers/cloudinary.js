5223f0a4245c1c476ba66260bd7d0120035ed3c1
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-07 13:31:03 -0400

    minor updates

---
+++ backend/helpers/cloudinary.js
@@ -0,0 +1,29 @@
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadMediaToCloudinary = async (filePath) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error uploading to cloudinary");
+  }
+};
+
+export const deleteMediaFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to delete assest from cloudinary");
+  }
+};
