75c329344ba6c54126873f7eabbfaa08fb5c02f7
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-07 01:59:44 -0400

    minor updates

--- frontend/src/components/instructorView/courses/AddNewCourse/CourseCurriculum.jsx
+++ frontend/src/components/instructorView/courses/AddNewCourse/CourseCurriculum.jsx
@@ -1,12 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { courseCurriculumInitialFormData } from "@/config";
 import { InstructorContext } from "@/context/instructorContext";
 import React, { useContext } from "react";
 
 const CourseCurriculum = () => {
   const { courseCurriculumFormData, setCourseCurriculumFormData } =
     useContext(InstructorContext);
+
+  // The function handleNewLecture is used to add a new lecture to the existing curriculum.
+  // It keeps the current lectures and adds a new lecture using the default data structure (courseCurriculumInitialFormData[0]).
+  // This helps manage the state for a form where users can add multiple lectures dynamically.
+  const handleNewLecture = () => {
+    setCourseCurriculumFormData([
+      ...courseCurriculumFormData,
+      {
+        ...courseCurriculumInitialFormData[0],
+      },
+    ]);
+  };
+
+  console.log(courseCurriculumFormData);
 
   return (
     <Card>
@@ -15,19 +32,28 @@ const CourseCurriculum = () => {
       </CardHeader>
 
       <CardContent>
-        <Button>Add Lecture</Button>
+        <Button onClick={handleNewLecture}>Add Lecture</Button>
         <div className="mt-4 space-y-4">
           {courseCurriculumFormData.map((curriculumItem, index) => (
-            <div className="border p-5 rounded-md">
+            <div className="border p-5 rounded-md" key={index}>
               <div className="flex gap-5 items-center">
                 <h3 className="font-semibold">Lecture {index + 1} </h3>
                 <Input
-                  name="title"
+                  name={`title-${index + 1}`}
                   placeholder="Enter lecture title"
                   className="max-w-96"
                 />
 
-                
+                <div className="flex items-center space-x-2">
+                  <Switch checked={true} id={`freePreview-${index + 1}`} />
+                  <Label htmlFor={`freePreview-${index + 1}`}>
+                    Free Preview
+                  </Label>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <Input type="file" accept="video/*" className="mb-4" />
               </div>
             </div>
           ))}
