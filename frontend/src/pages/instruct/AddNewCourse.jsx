7587563b2e9b035da701218b3bfa6565963fd085
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-13 08:18:45 -0400

    minor updates

--- frontend/src/pages/instruct/AddNewCourse.jsx
+++ frontend/src/pages/instruct/AddNewCourse.jsx
@@ -1,44 +1,86 @@
-import CourseCurriculum from '@/components/instructorView/courses/AddNewCourse/CourseCurriculum'
-import CourseLanding from '@/components/instructorView/courses/AddNewCourse/CourseLanding'
-import CourseSetting from '@/components/instructorView/courses/AddNewCourse/CourseSetting'
-import { Button } from '@/components/ui/button'
-import { Card, CardContent } from '@/components/ui/card'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import React from 'react'
+import CourseCurriculum from "@/components/instructorView/courses/AddNewCourse/CourseCurriculum";
+import CourseLanding from "@/components/instructorView/courses/AddNewCourse/CourseLanding";
+import CourseSetting from "@/components/instructorView/courses/AddNewCourse/CourseSetting";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { InstructorContext } from "@/context/instructorContext";
+import React, { useContext } from "react";
 
 const AddNewCourse = () => {
+  const { courseLandingFormData, courseCurriculumFormData } =
+    useContext(InstructorContext);
+
+  function isEmpty(value) {
+    // The function first checks if the value is an array using the built-in method Array.isArray()
+    if (Array.isArray(value)) {
+      return value.length === 0;
+    }
+    return value === "" || value === null || value === undefined;
+  }
+
+  function validateFormData() {
+    for (const key in courseLandingFormData) {
+      if (isEmpty(courseLandingFormData[key])) {
+        return false;
+      }
+    }
+
+    let hasFreePreview = false;
+    for (const item of courseCurriculumFormData) {
+      if (
+        isEmpty(item.title) ||
+        isEmpty(item.videoUrl) ||
+        isEmpty(item.public_id)
+      ) {
+        return false;
+      }
+      if(item.freePreview) {
+        hasFreePreview = true  // found at least one free preview
+      }
+    }
+    return hasFreePreview
+  }
+
   return (
-    <div className='container mx-auto p-4'>
-      <div className='flex justify-between'>
-        <h1 className='text-3xl font-extrabold mb-5'>Create new course</h1>
-        <Button className="text-sm tracking-wider font-bold px-8">Submit</Button>
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-extrabold mb-5">Create new course</h1>
+        <Button
+          disabled={!validateFormData()}
+          className="text-sm tracking-wider font-bold px-8"
+        >
+          Submit
+        </Button>
       </div>
 
       <Card>
         <CardContent>
-            <div className='container mx-auto p-4'>
-                <Tabs defaultValue='curriculum' className='space-y-4'>
-                    <TabsList>
-                        <TabsTrigger value="curriculum">curriculum</TabsTrigger>
-                        <TabsTrigger value="course-landing-page">course-landing-page</TabsTrigger>
-                        <TabsTrigger value="settings">settings</TabsTrigger>
-                    </TabsList>
+          <div className="container mx-auto p-4">
+            <Tabs defaultValue="curriculum" className="space-y-4">
+              <TabsList>
+                <TabsTrigger value="curriculum">curriculum</TabsTrigger>
+                <TabsTrigger value="course-landing-page">
+                  course-landing-page
+                </TabsTrigger>
+                <TabsTrigger value="settings">settings</TabsTrigger>
+              </TabsList>
 
-                    <TabsContent value="curriculum">
-                        <CourseCurriculum />
-                    </TabsContent>
-                    <TabsContent value="course-landing-page">
-                        <CourseLanding />
-                    </TabsContent>
-                    <TabsContent value="settings">
-                        <CourseSetting />
-                    </TabsContent>
-                </Tabs>
-            </div>
+              <TabsContent value="curriculum">
+                <CourseCurriculum />
+              </TabsContent>
+              <TabsContent value="course-landing-page">
+                <CourseLanding />
+              </TabsContent>
+              <TabsContent value="settings">
+                <CourseSetting />
+              </TabsContent>
+            </Tabs>
+          </div>
         </CardContent>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default AddNewCourse
+export default AddNewCourse;
