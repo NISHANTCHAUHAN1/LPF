1a149b66276bceb5d660a97f5afb1f293b4e7971
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-16 14:13:23 -0400

    minor updates

--- frontend/src/components/studentView/CommonLayout.jsx
+++ frontend/src/components/studentView/CommonLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from './Header'
 
 const CommonLayout = () => {
   return (
     <div>
-      CommonLayout CONTENT
+      <Header />
       <Outlet />
     </div>
   )
