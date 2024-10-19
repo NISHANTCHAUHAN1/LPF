87f91c973901d2d278988dc819a3a221619e3b05
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-18 20:26:50 -0400

    minor updates

--- backend/controllers/studentController/OrderController.js
+++ backend/controllers/studentController/OrderController.js
@@ -1,5 +1,7 @@
 import { Order } from "../../models/Order.js";
 import paypal from "paypal-rest-sdk";
+import { StudentCourses } from "../../models/StudentCourses.js";
+import { Course } from "../../models/Course.js";
 
 export const createOrder = async (req, res) => {
   try {
@@ -105,3 +107,85 @@ export const createOrder = async (req, res) => {
   }
 };
 
+
+export const capturePaymentAndFinalizeOrder = async (req, res) => {
+  try {
+    const { paymentId, payerId, orderId } = req.body;
+
+    let order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order can not be found",
+      });
+    }
+
+    order.paymentStatus = "paid";
+    order.orderStatus = "confirmed";
+    order.paymentId = paymentId;
+    order.payerId = payerId;
+
+    await order.save();
+
+    // now update student course model
+    const studentCourses = await StudentCourses.findOne({
+      userId: order.userId,
+    });
+
+    // if i buy first time any course like react js then else block code is run
+    // if i buy 2 time a course after react js then if code is run;
+    if (studentCourses) {
+      studentCourses.courses.push({
+        courseId: order.courseId,
+        title: order.courseTitle,
+        instructorId: order.instructorId,
+        instructorName: order.instructorName,
+        dateOfPurchase: order.orderDate,
+        courseImage: order.courseImage,
+      });
+      await studentCourses.save();
+
+    } else {
+      const newStudentCourses = new StudentCourses({
+        userId: order.userId,
+        courses: [
+          {
+            courseId: order.courseId,
+            title: order.courseTitle,
+            instructorId: order.instructorId,
+            instructorName: order.instructorName,
+            dateOfPurchase: order.orderDate,
+            courseImage: order.courseImage,
+          },
+        ],
+      });
+      await newStudentCourses.save();
+    }
+
+    //update the course schema To see how many students have bought the course and who they are.
+    await Course.findByIdAndUpdate(order.courseId, {
+      $addToSet: {
+        students : {
+          studentId: order.userId,
+          studentName: order.userName,
+          studentEmail: order.userEmail,
+          paidAmount: order.coursePricing,
+        }
+      }
+    })
+
+    res.status(200).json({
+      success: true,
+      message: "Order confirmed",
+      data: order,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
