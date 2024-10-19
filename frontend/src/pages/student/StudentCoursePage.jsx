aed64224fd6baa11b53a651c1f1066420030cde0
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-19 17:28:00 -0400

    minor updates

--- frontend/src/pages/student/StudentCoursePage.jsx
+++ frontend/src/pages/student/StudentCoursePage.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import { Card } from "@/components/ui/card";
+import { AuthContext } from "@/context/authContext";
+import { StudentContext } from "@/context/studentContext";
+import { fetchStudentBoughtCoursesService } from "@/services";
+import React, { useContext, useEffect } from "react";
 
 const StudentCoursePage = () => {
+  const { studentBoughtCoursesList, setStudentBoughtCoursesList } =
+    useContext(StudentContext);
+  const { auth } = useContext(AuthContext);
+
+  async function fetchStudentBoughtCourses() {
+    const response = await fetchStudentBoughtCoursesService(auth?.user?._id);
+    if (response?.success) {
+      setStudentBoughtCoursesList(response?.data);
+    }
+    console.log(response);
+  }
+
+  useEffect(() => {
+    fetchStudentBoughtCourses();
+  }, []);
+
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold mb-8">My Courses</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5"></div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {studentBoughtCoursesList && studentBoughtCoursesList.length > 0 ? (
+          <Card></Card>
+        ) : (
+          <h1 className="text-3xl font-bold">No Courses found</h1>
+        )}
+      </div>
     </div>
   );
 };
