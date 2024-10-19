ad553c641bbd36d23aef35024704687499511304
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-19 17:47:07 -0400

    minor updates

--- frontend/src/pages/student/StudentCoursePage.jsx
+++ frontend/src/pages/student/StudentCoursePage.jsx
@@ -1,13 +1,18 @@
-import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { AuthContext } from "@/context/authContext";
 import { StudentContext } from "@/context/studentContext";
 import { fetchStudentBoughtCoursesService } from "@/services";
+import { Watch } from "lucide-react";
 import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const StudentCoursePage = () => {
   const { studentBoughtCoursesList, setStudentBoughtCoursesList } =
     useContext(StudentContext);
   const { auth } = useContext(AuthContext);
+
+  const navigate = useNavigate();
 
   async function fetchStudentBoughtCourses() {
     const response = await fetchStudentBoughtCoursesService(auth?.user?._id);
@@ -26,7 +31,33 @@ const StudentCoursePage = () => {
       <h1 className="text-3xl font-bold mb-8">My Courses</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {studentBoughtCoursesList && studentBoughtCoursesList.length > 0 ? (
-          <Card></Card>
+          studentBoughtCoursesList.map((course, index) => (
+            <Card key={index} className="flex flex-col">
+              <CardContent className="p-4 flex-grow">
+                <img
+                  src={course?.courseImage}
+                  alt={course?.title}
+                  className="h-52 w-full object-cover rounded-md mb-4"
+                />
+                <h3 className="font-bold mb-1">{course?.title}</h3>
+                <p className="text-sm text-gray-700 mb-2">
+                  {course?.instructorName}
+                </p>
+              </CardContent>
+
+              <CardFooter>
+              <Button
+              onClick={() =>
+                navigate(`/course-progress/${course?.courseId}`)
+              }
+                  className="flex-1"
+                >
+                  <Watch className="mr-2 h-4 w-4" />
+                  Start Watching
+                </Button>
+              </CardFooter>
+            </Card>
+          ))
         ) : (
           <h1 className="text-3xl font-bold">No Courses found</h1>
         )}
