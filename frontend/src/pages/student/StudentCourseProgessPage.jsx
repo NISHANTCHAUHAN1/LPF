8470d94512dd4cd7a1162647ecea2e410a64c479
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-20 18:37:13 -0400

    minor updates

--- frontend/src/pages/student/StudentCourseProgessPage.jsx
+++ frontend/src/pages/student/StudentCourseProgessPage.jsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import VideoPlayer from "@/components/videoPlayer/indedx";
 import { AuthContext } from "@/context/authContext";
 import { StudentContext } from "@/context/studentContext";
 import { getCurrentCourseProgressService } from "@/services";
-import { ChevronLeft } from "lucide-react";
+import { Check, ChevronLeft, ChevronRight, Play } from "lucide-react";
 import React, { useContext, useEffect, useState } from "react";
 // import Confetti from "react-confetti";
 import Confetti from "react-confetti";
@@ -21,6 +30,7 @@ const StudentCourseProgessPage = () => {
   const [showCourseCompleteDialog, setShowCourseCompleteDialog] =
     useState(false);
   const [showConfetti, setShowConfetti] = useState(false);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
 
   async function fetchCurrentCourseProgress() {
     const response = await getCurrentCourseProgressService(auth?.user?._id, id);
@@ -40,6 +50,10 @@ const StudentCourseProgessPage = () => {
           setShowConfetti(true);
           return;
         }
+
+        if (response?.data?.progress?.length === 0) {
+          setCurrentLecture(response?.data?.courseDetails?.curriculum[0]);
+        }
       }
     }
   }
@@ -52,31 +66,114 @@ const StudentCourseProgessPage = () => {
     if (showConfetti) setTimeout(() => setShowConfetti(false), 15000);
   }, [showConfetti]);
 
+  console.log(studentCurrentCourseProgress, "studentCurrentCourseProgress");
+
   return (
     <div className="flex flex-col h-screen bg-[#1c1d1f] text-white">
-      {
-        showConfetti && <Confetti />
-      }
-       {/* {showConfetti && <Confetti />} */}
+      {showConfetti && <Confetti />}
       <div className="flex items-center justify-between p-4 bg-[#1c1d1f] border-b border-gray-700">
         <div className="flex items-center space-x-4">
           <Button
             onClick={() => navigate("/student-courses")}
-            className="text-black"
+            className="text-white"
             variant="ghost"
             size="sm"
           >
             <ChevronLeft className="h-4 w-4 mr-2" />
             Back to My Courses Page
           </Button>
-          <h1 className="text-lg font-bold hidden md:block">
-            student title course
+          <h1 className="text-lg font-bold truncate hidden md:block">
+            {studentCurrentCourseProgress?.courseDetails?.title}
           </h1>
+        </div>
+        <Button onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+          {isSideBarOpen ? (
+            <ChevronRight className="h-5 w-5" />
+          ) : (
+            <ChevronLeft className="h-5 w-5" />
+          )}
+        </Button>
+      </div>
+
+      <div className="flex flex-1 overflow-hidden">
+        <div
+          className={`flex-1 ${
+            isSideBarOpen ? "mr-[400px]" : ""
+          } transition-all duration-300`}
+        >
+          <VideoPlayer
+            width="100%"
+            height="500px"
+            url={currentLecture?.videoUrl}
+            onProgressUpdate={setCurrentLecture}
+            progressData={currentLecture}
+          />
+          <div className="p-6 bg-[#1c1d1f]">
+            <h2 className="text-2xl font-bold mb-2">{currentLecture?.title}</h2>
+          </div>
+        </div>
+
+        <div
+          className={`fixed top-[64px] right-0 bottom-0 w-[400px] bg-[#1c1d1f] border-l border-gray-700 transition-transform duration-300 ${
+            isSideBarOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <Tabs defaultValue="content" className="h-full flex flex-col">
+            <TabsList className="grid bg-gray-800 w-full grid-cols-2 p-0 h-14">
+              <TabsTrigger
+                value="content"
+                className="text-white rounded-none h-full"
+              >
+                Course Content
+              </TabsTrigger>
+              <TabsTrigger
+                value="overview"
+                className="text-white rounded-none h-full"
+              >
+                Overview
+              </TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="content">
+              <ScrollArea className="flex-1 h-full overflow-y-auto">
+                <div className="p-4 space-y-4">
+                  {studentCurrentCourseProgress?.courseDetails?.curriculum.map(
+                    (item) => (
+                      <div
+                        className="flex items-center space-x-2 text-sm text-white font-bold cursor-pointer"
+                        key={item._id}
+                      >
+                        {studentCurrentCourseProgress?.progress?.find(
+                          (progressItem) => progressItem.lectureId === item._id
+                        )?.viewed ? (
+                          <Check className="h-4 w-4 text-green-500" />
+                        ) : (
+                          <Play className="h-4 w-4" />
+                        )}
+                        <span>{item?.title}</span>
+                      </div>
+                    )
+                  )}
+                </div>
+              </ScrollArea>
+            </TabsContent>
+
+            <TabsContent value="overview" className="flex-1 overflow-hidden">
+              <ScrollArea className="h-full">
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-4">About this course</h2>
+                  <p className="text-gray-400">
+                    {studentCurrentCourseProgress?.courseDetails?.description}
+                  </p>
+                </div>
+              </ScrollArea>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
 
       <Dialog open={lockCourse}>
-        <DialogContent className="sm:w-[425px]">
+        <DialogContent className="w-full max-w-sm mx-auto p-6 sm:w-[425px]">
           <DialogHeader>
             <DialogTitle>You can't view this page</DialogTitle>
             <DialogDescription>
@@ -85,17 +182,24 @@ const StudentCourseProgessPage = () => {
           </DialogHeader>
         </DialogContent>
       </Dialog>
+
       <Dialog open={showCourseCompleteDialog}>
-        <DialogContent showOverlay={false} className="sm:w-[425px]">
+        <DialogContent
+          showOverlay={false}
+          className="w-full max-w-sm mx-auto p-6 sm:w-[425px]"
+        >
           <DialogHeader>
             <DialogTitle>Congratulations!</DialogTitle>
             <DialogDescription className="flex flex-col gap-3">
               <Label>You have completed the course</Label>
               <div className="flex flex-row gap-3">
-                <Button onClick={() => navigate("/student-courses")}>
+                <Button
+                  className="px-4 py-2"
+                  onClick={() => navigate("/student-courses")}
+                >
                   My Courses Page
                 </Button>
-                <Button >Rewatch Course</Button>
+                <Button className="px-4 py-2">Rewatch Course</Button>
               </div>
             </DialogDescription>
           </DialogHeader>
