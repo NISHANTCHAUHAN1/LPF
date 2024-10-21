11f1926527b5ddeb7aab4245d56eacaf51b14d99
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-21 01:55:50 -0400

    minor updates

--- backend/routes/studentRoute/CourseProgessRoute.js
+++ backend/routes/studentRoute/CourseProgessRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getCurrentCourseProgress } from "../../controllers/studentController/CourseProgressCont.js";
+import { getCurrentCourseProgress, markCurrentLectureAsViewed, resetCurrentCourseProgress } from "../../controllers/studentController/CourseProgressCont.js";
 
 const router = express.Router();
 
+router.post("/mark-lecture-viewed", markCurrentLectureAsViewed);
 router.get("/get/:userId/:courseId", getCurrentCourseProgress);
+router.post("/reset-progress", resetCurrentCourseProgress);
 
 export default router;
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -125,3 +125,26 @@ export async function getCurrentCourseProgressService(userId, courseId) {
 
   return data;
 }
+
+export async function markCurrentLectureAsViewed(userId, courseId, lectureId) {
+  const { data } = await axiosInstance.post(
+    `/student/course-progress/mark-lecture-viewed`,
+    {
+      userId,
+      courseId,
+      lectureId,
+    }
+  );
+  return data;
+}
+
+export async function resetLectuirview(userId, courseId) {
+  const { data } = await axiosInstance.post(
+    `/student/course-progress/reset-progress`,
+    {
+      userId,
+      courseId,
+    }
+  );
+  return data;
+}
