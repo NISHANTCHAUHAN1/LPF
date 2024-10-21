d236437f0a02eb55b24a606cb0ecbda1e7dca094
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-10-21 01:45:21 -0400

    minor updates

--- backend/controllers/studentController/CourseProgressCont.js
+++ backend/controllers/studentController/CourseProgressCont.js
@@ -63,7 +63,7 @@ export const markCurrentLectureAsViewed = async (req, res) => {
       message: "Lecture marked as viewed",
       data: progress,
     });
-    
+
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -145,3 +145,37 @@ export const getCurrentCourseProgress = async (req, res) => {
 };
 
 // reset course progress
+
+export const  resetCurrentCourseProgress = async (req, res) => {
+  try {
+    const { userId, courseId } = req.body;
+
+    const progress = await CourseProgress.findOne({ userId, courseId });
+
+    if (!progress) {
+      return res.status(404).json({
+        success: false,
+        message: "Progress not found!",
+      });
+    }
+
+    progress.lecturesProgress = [];
+    progress.completed = false;
+    progress.completionDate = null;
+
+    await progress.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Course progress has been reset",
+      data: progress
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
